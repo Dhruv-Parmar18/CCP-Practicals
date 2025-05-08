f644ce7dd268c56bc557bacc6917d9709777e869
Dhruv Parmar <24ce071@charusat.edu.in>
2025-05-08 16:39:02 +0530

    Add files via upload

---
+++ 8_2/8_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+
+    map<string, int> wordFrequency;
+    stringstream ss(sentence);
+    string word;
+
+    while (ss >> word)
+    {
+        ++wordFrequency[word];
+    }
+
+    cout << "\nWord Frequency:\n";
+    for (const auto &pair : wordFrequency)
+    {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+    cout << "\n\nDhruv Parmar" << endl
+         << "24CE071" << endl;
+
+    return 0;
+}
