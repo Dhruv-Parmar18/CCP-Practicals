424bac955ae697d84f85f5af8e3f681af093fe5f
Dhruv Parmar <24ce071@charusat.edu.in>
2025-05-08 16:39:18 +0530

    Add files via upload

---
+++ 8_3/8_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> uniqueTransactions;
+    int transactionId;
+
+    cout << "Enter transaction IDs (0 to stop): ";
+    while (cin >> transactionId && transactionId != 0)
+    {
+        uniqueTransactions.insert(transactionId);
+    }
+
+    cout << "\nUnique Sorted Transaction IDs:\n";
+    for (int id : uniqueTransactions)
+    {
+        cout << id << " ";
+    }
+
+    cout << endl;
+
+    cout << "\n\nDhruv Parmar" << endl
+         << "24CE071" << endl;
+
+    return 0;
+}
